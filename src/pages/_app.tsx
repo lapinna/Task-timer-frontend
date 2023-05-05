8dee6f9cc811fb68d813dd2c7e2e31301b8be0f2
Lapina <lapinna.inese@gmail.com>
2023-05-05 11:55:30 +0300

    add initial graphql

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,10 @@
+import { client } from '@/graphql/apollo-client'
+import { ApolloProvider } from '@apollo/client'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return 
+  <ApolloProvider client={client}>
+  <Component {...pageProps} />
+  </ApolloProvider>
 }
