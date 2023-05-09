fa56995b739997954ee77cd89b6a9760bcec9902
Lapina <lapinna.inese@gmail.com>
2023-05-09 16:08:58 +0300

    update graphql add login mutation

--- src/graphql/operations/user.tsx
+++ src/graphql/operations/user.tsx
@@ -9,17 +9,29 @@ export default {
         username
         email
         }
-      }`
+      }
+      `,
   },
 
   Mutations: {
     REGISTER_USER: gql`
     mutation RegisterUser($input: RegisterInput!) {
-        registerUser(input: $input) {
-          username
-          email
-          password
-          confirmPassword
+      registerUser(input: $input) {
+        username
+        email
+        password
+        confirmPassword
+        }
+      }
+    `,
+
+    LOGIN_USER: gql`
+    mutation LoginUser($input: LoginInput!) {
+      loginUser(input: $input) {
+        userId
+        username
+        token
+        tokenExpire
         }
       }
     `
