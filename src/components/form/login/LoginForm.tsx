f3bdf9835991836f4132c57673d6ec90ddb7a3c1
Lapina <lapinna.inese@gmail.com>
2023-05-10 12:15:04 +0300

    fix login form, add profile page

--- src/components/form/login/LoginForm.tsx
+++ src/components/form/login/LoginForm.tsx
@@ -1,41 +1,56 @@
-import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { useMutation } from "@apollo/client";
+import { useRouter } from 'next/router';
 import styles from "../Form.module.scss";
 import RegisterForm from "../register/RegisterForm";
+import UserOperations from "../../../graphql/operations/user"
 
 interface AuthProps {
   isLogin: boolean;
   toogle: () => void;
 }
 
-const LoginForm: React.FC<AuthProps> = ({ isLogin, toogle }) => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+type LoginFormValues = {
+  email: string;
+  password: string;
+}
 
-  const handleSubmit = (e: React.FormEvent) => {
-    e.preventDefault();
-    alert("LOGIN SUCCESSFUL!")
+const LoginForm: React.FC<AuthProps> = ({ isLogin, toogle }) => {
+  const router = useRouter();
+
+  const { register, handleSubmit } = useForm<LoginFormValues>();
+
+  const [loginUser] = useMutation(UserOperations.Mutations.LOGIN_USER)
+
+  const submitData = async ({ email, password }: LoginFormValues) => {
+    try {
+      await loginUser({ variables: { email, password } });
+      router.push("/profile");
+    } catch (error) {
+      console.error(error)
+    }
   };
 
   return (
     <div className={styles.formWrapper}>
       {isLogin ? (
-        <form onSubmit={handleSubmit}>
+        <form onSubmit={handleSubmit(submitData)}>
           <h2>Welcome Back!</h2>
           <fieldset>
             <legend>Log In</legend>
             <div>
               <div>
                 <label htmlFor="email">Email:</label>
-                <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
+                <input type="email" {...register("email")} required />
               </div>
               <div>
                 <label htmlFor="password">Password:</label>
-                <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
+                <input type="password" {...register("password")} required />
               </div>
             </div>
           </fieldset>
           <button type="submit">Login</button>
-          <button onClick={() => toogle()}>Create an Account</button>
+          <button type="button" onClick={() => toogle()}>Create an Account</button>
         </form>
       ) : (
         <RegisterForm btnHaveAccount={() => toogle()} />
