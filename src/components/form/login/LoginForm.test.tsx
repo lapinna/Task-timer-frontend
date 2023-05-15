2dc254d6b86e9cf079e430499ec2caaaa6c23a59
Lapina <lapinna.inese@gmail.com>
2023-05-15 18:38:42 +0300

    update login form unit tests

--- src/components/form/login/LoginForm.test.tsx
+++ src/components/form/login/LoginForm.test.tsx
@@ -1,8 +1,10 @@
+import "@testing-library/jest-dom";
 import { fireEvent, render, screen, waitFor } from "@testing-library/react";
 import mockRouter from 'next-router-mock';
 import { MockedProvider } from "@apollo/client/testing";
-import LoginForm from "./LoginForm";
 import { act } from "react-dom/test-utils";
+import LoginForm from "./LoginForm";
+import userLoginMutation from "../../../graphql/operations/user";
 
 jest.mock("next/router", () => require("next-router-mock"));
 
@@ -10,22 +12,21 @@ jest.mock("react-hook-form", () => ({
   ...jest.requireActual("react-hook-form"),
 }));
 
-const setup = () => {
-  const wrapper = render(
-    <MockedProvider>
-      <LoginForm />
-    </MockedProvider>
-  );
-  return wrapper;
-};
+const mockLogin = jest.fn();
 
 describe("LoginForm component", () => {
   beforeEach(() => {
     jest.clearAllMocks();
+    act(() => {
+      render(
+        <MockedProvider>
+          <LoginForm />
+        </MockedProvider>
+      );
+    })
   });
 
   test("renders login form", () => {
-    setup();
     expect(screen.getByTestId("loginForm")).toBeTruthy();
     expect(screen.getByTestId("emailInput")).toBeTruthy();
     expect(screen.getByTestId("passwordInput")).toBeTruthy();
@@ -34,29 +35,37 @@ describe("LoginForm component", () => {
   });
 
   test('mocks the useRouter hook', () => {
-    setup();
     act(() => {
       mockRouter.push("");
     });
   });
 
-  test("submits login form properly", async () => {
-    const mockLogin = jest.fn();
-    setup();
-    const emailInput = await screen.findByTestId("emailInput");
-    const passwordInput = screen.getByTestId("passwordInput");
-    const loginButton = await screen.findByTestId("loginBtn");
-    fireEvent.change(emailInput, { target: { values: "test@gmail.com" } });
-    fireEvent.change(passwordInput, { target: { values: "1234567" } });
-    fireEvent.click(loginButton);
-    mockLogin(emailInput, passwordInput);
+  test("should not login if inputs empty", async () => {
+    const loginButton = screen.getByTestId("loginBtn");
+    fireEvent.submit(loginButton);
+    expect(mockLogin).not.toBeCalled();
+  });
+
+  test("should allow fill inputs", async () => {
+    const email = screen.getByTestId("emailInput");
+    const password = screen.getByTestId("passwordInput");
+    fireEvent.change(email, { target: { value: "email@test.com" } });
+    fireEvent.change(password, { target: { value: "test1" } });
     await waitFor(() => {
-      expect(mockLogin).toHaveBeenCalledWith({
-        variables: {
-          email: "test@gmail.com",
-          password: "1234567",
-        },
-      });
+      expect(email).toHaveDisplayValue("email@test.com");
+      expect(password).toHaveDisplayValue("test1");
+    }); 
+  });     
+
+  test("submits login form properly", async () => {
+    const emailInput = screen.getByTestId("emailInput");
+    const passwordInput = screen.getByTestId("passwordInput");
+    const loginButton = screen.getByTestId("loginBtn");
+    fireEvent.change(emailInput, { target: { value: "email@test.com" } });
+    fireEvent.change(passwordInput, { target: { value: "test1" } });
+    fireEvent.submit(loginButton);
+    await waitFor(() => {
+      expect(mockRouter.push("/profile"));
     })
   });
 });
