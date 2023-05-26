a74f8b9e62cf9b8af658aec765569becf33a8864
Lapina <lapinna.inese@gmail.com>
2023-05-26 16:55:24 +0300

    refactor navBar, update searchbar

--- src/components/navBar/NavBar.tsx
+++ src/components/navBar/NavBar.tsx
@@ -1,15 +1,13 @@
 import { useRouter } from "next/router";
 import styles from "./NavBar.module.scss";
-import SearchBar from "../searchBar/SearchBar";
 import { useContext, useState } from "react";
-import Searchresult from "../SearchResult/Searchresult";
 import { AuthContext } from "@/context/AuthContext";
 
 const NavBar = () => {
   const { user, logout } = useContext(AuthContext);
   const router = useRouter();
 
-  const [results, setResults] = useState<string[]>([]);
+  const username = user?.username
 
   const handleLogout = () => {
     try {
@@ -23,19 +21,8 @@ const NavBar = () => {
   return (
     <div className={styles.navBarWrapper}>
       <div>
-        <h2>Hello, {user.username}</h2>
+        <h2>Hello, {username}</h2>
         <button>Update</button>
-      </div>
-      <SearchBar results={results} setResults={setResults} />
-      <div>
-        {
-          results.map((result, index) => {
-            return <Searchresult result={result} key={index} />
-          })
-        }
-      </div>
-      <div>
-        <button>Add new</button>
       </div>
       <div>
         <button onClick={handleLogout}>Logout</button>
--- src/components/searchBar/SearchBar.tsx
+++ src/components/searchBar/SearchBar.tsx
@@ -3,31 +3,51 @@ import { BsSearch } from "react-icons/bs";
 import styles from "./SearchBar.module.scss";
 import { AuthContext } from "@/context/AuthContext";
 
-const SearchBar = ({ setResults }: any) => {
+const SearchBar = () => {
   const [searchInput, setSearchInput] = useState("");
-    const { user } = useContext(AuthContext);
-    const tasks = user.tasks;
+  const { user } = useContext(AuthContext);
+  const tasks = user?.tasks
 
-  // if (searchInput.length > 0) {
-  //   tasks.filter((task: any) => {
-  //     return task.title.includes(searchInput);
-  //   });
-  // };
+  const filteredTasks = tasks?.filter((task: any) => {
+    return task.title.includes(searchInput)
+  });
 
-  const handleChange = (value: any) => {
-    setSearchInput(value);
-    setResults(tasks);
+  const handleInput: React.ChangeEventHandler<HTMLInputElement> = (e) => {
+    setSearchInput(e.target.value);
   };
 
   return (
-    <div className={styles.searchBarWrapper}>
-      <BsSearch />
-      <input
-        onChange={(e) => handleChange(e.target.value)}
-        value={searchInput}
-        type="search"
-        placeholder="Type to search..."
-      />
+    <div className={styles.searchWrapper}>
+      <div className={styles.searchBarWrapper}>
+        <BsSearch />
+        <input
+          onChange={handleInput}
+          value={searchInput}
+          type="search"
+          placeholder="Type to search..."
+        />
+      </div>
+      {/* {searchInput &&
+        <div className={styles.searchResultWrapper}>
+          <ul>
+            {
+              filteredTasks?.map((result: any, index: any) => {
+                return (
+                  <div key={index} >
+                    <li><a href="#">{result.title}</a></li>
+                    <span>spentTime</span>
+                    <div>
+                      <button>Start</button>
+                      <button>Pause</button>
+                      <button>Stop</button>
+                    </div>
+                  </div>
+                )
+              })
+            }
+          </ul>
+        </div>
+      } */}
     </div>
   );
 };
