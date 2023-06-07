a76d46691a126423e179ed3d18e60e0422f7c477
Lapina <lapinna.inese@gmail.com>
2023-06-07 10:29:32 +0300

    add tasks query

--- src/graphql/operations/task.tsx
+++ src/graphql/operations/task.tsx
@@ -1,6 +1,19 @@
 import { gql } from "@apollo/client";
 
 export default {
+  Query: {
+    GET_TASKS: gql`
+    query GetTasks($userId: String!) {
+      getTasks(userId: $userId) {
+        id
+        title
+        author
+        timeSpent
+        createdAt
+      }
+    }
+    `,
+  },
   Mutations: {
     CREATE_TASK: gql`
     mutation CreateTask($userId: String!, $title: String!) {
